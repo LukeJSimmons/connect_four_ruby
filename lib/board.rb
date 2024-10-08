4d3fc709088da38d8c23110b41ea9224e8c0127f
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-10-08 12:50:47 -0500

    feat: place, take_input, check_for_win, win, and play functions added

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,120 @@
 class Board
   def initialize(board=[
-    ['#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#'],
+    ['☐','☐','☐','☐','☐','☐','☐'],
+    ['☐','☐','☐','☐','☐','☐','☐'],
+    ['☐','☐','☐','☐','☐','☐','☐'],
+    ['☐','☐','☐','☐','☐','☐','☐'],
+    ['☐','☐','☐','☐','☐','☐','☐'],
+    ['☐','☐','☐','☐','☐','☐','☐'],
   ])
     @board = board
+    @current_symbol = '☑'
+  end
+
+  def play
+    print
+    take_input
+    until check_for_win
+      play
+    end
   end
   
-  def print
+  def print(board=@board)
     board_str = ''
     
-    @board.each do |row|
+    board.each do |row|
       board_str << "\n"
       board_str << row.join(' ')
     end
 
     puts board_str
+  end
+
+  def place(col, symbol)
+    col -= 1 # Adjusts to array zero floor
+
+    deepest_empty_spot = 5
+    @board.each_with_index { |row, index| deepest_empty_spot = index if row[col] == '☐' }
+
+    @board[deepest_empty_spot][col] = symbol
+  end
+
+  def take_input
+    col_input = gets.chomp
+
+    exit if col_input == 'exit' || col_input == 'q'
+
+    if col_input.to_i.to_s != col_input || col_input.to_i >= 7 || col_input.to_i <= 0
+      puts "Invalid Input: Please input a number between 1 and 7"
+      return
+    end
+
+    place(col_input.to_i, @current_symbol)
+    @current_symbol = @current_symbol == '☑' ? '☒' : '☑'
+  end
+
+  def check_for_win
+    # Horizontal wins
+    if @board.any? { |row| row[0..3].all? { |item| item == '☑' } || row[1..4].all? { |item| item == '☑' } || row[2..5].all? { |item| item == '☑' } || row[3..6].all? { |item| item == '☑' } }
+      win('☑')
+    elsif @board.any? { |row| row[0..3].all? { |item| item == '☒' } || row[1..4].all? { |item| item == '☒' } || row[2..5].all? { |item| item == '☒' } || row[3..6].all? { |item| item == '☒' } }
+      win('☒')
+    end
+
+    # Vertical Wins
+    i = 0
+    until i == 7 do
+      column = []
+
+      j = 0
+      until j == 6 do
+        column << @board[j][i]
+        j += 1
+      end
+
+      if column[0..3].all? { |item| item == '☑' } || column[1..4].all? { |item| item == '☑' } || column[2..5].all? { |item| item == '☑' }
+        win('☑')
+      elsif column[0..3].all? { |item| item == '☒' } || column[1..4].all? { |item| item == '☒' } || column[2..5].all? { |item| item == '☒' }
+        win('☒')
+      end
+
+      i += 1
+    end
+
+    # Diaganol Wins
+    forword_diaganols = [
+      [@board[2][0],@board[3][1],@board[4][2],@board[5][3],'☐','☐'],
+      [@board[1][0],@board[2][1],@board[3][2],@board[4][3],@board[5][4],'☐'],
+      [@board[0][0],@board[1][1],@board[2][2],@board[3][3],@board[4][4],@board[5][5]],
+      [@board[0][1],@board[1][2],@board[2][3],@board[3][4],@board[4][5],@board[5][6]],
+      [@board[0][2],@board[1][3],@board[2][4],@board[3][5],@board[4][6],'☐'],
+      [@board[0][3],@board[1][4],@board[2][5],@board[3][6],'☐','☐'],
+    ]
+
+    backward_diaganols = [
+      [@board[2][6],@board[3][5],@board[4][4],@board[5][3],'☐','☐'],
+      [@board[1][6],@board[2][5],@board[3][4],@board[4][3],@board[5][2],'☐'],
+      [@board[0][6],@board[1][5],@board[2][4],@board[3][3],@board[4][2],@board[5][1]],
+      [@board[0][5],@board[1][4],@board[2][3],@board[3][2],@board[4][1],@board[5][0]],
+      [@board[0][4],@board[1][3],@board[2][2],@board[3][1],@board[4][0],'☐'],
+      [@board[0][3],@board[1][2],@board[2][1],@board[3][0],'☐','☐'],
+    ]
+
+    if forword_diaganols.any? { |row| row[0..3].all? { |item| item == '☑' } || row[1..4].all? { |item| item == '☑' } || row[2..5].all? { |item| item == '☑' } }
+      win('☑')
+    elsif forword_diaganols.any? { |row| row[0..3].all? { |item| item == '☒' } || row[1..4].all? { |item| item == '☒' } || row[2..5].all? { |item| item == '☒' } }
+      win('☒')
+    elsif backward_diaganols.any? { |row| row[0..3].all? { |item| item == '☑' } || row[1..4].all? { |item| item == '☑' } || row[2..5].all? { |item| item == '☑' } }
+      p 'bd'
+      win('☑')
+    elsif backward_diaganols.any? { |row| row[0..3].all? { |item| item == '☒' } || row[1..4].all? { |item| item == '☒' } || row[2..5].all? { |item| item == '☒' } }
+      win('☒')
+    end
+  end
+
+  def win(symbol)
+    print
+    puts symbol == '☑' ? "\n☑ Connected Four!" : "\n☒ Connected Four!"
+    exit
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,11 +3,338 @@ require './lib/board'
 describe Board do
   subject(:board) { described_class.new }
 
+  before do
+    allow(board).to receive(:exit)
+  end
+
   describe '#print' do
     context 'when board is empty' do
       it 'displays empty board' do
-        expect(board).to receive(:puts).with("\n# # # # # # #\n# # # # # # #\n# # # # # # #\n# # # # # # #\n# # # # # # #\n# # # # # # #")
+        expect(board).to receive(:puts).with("\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐")
         board.print
+      end
+    end
+
+    context 'when board is not empty' do
+      subject(:board) { described_class.new([
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☑','☐','☐','☒','☐','☐'],
+      ]) }
+
+      it 'displays board correctly' do
+        expect(board).to receive(:puts).with("\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☐ ☐ ☐ ☐ ☐ ☐\n☐ ☑ ☐ ☐ ☒ ☐ ☐")
+        board.print
+      end
+    end
+  end
+
+  describe '#place' do
+    context 'when column is empty' do
+      it 'drops circle to bottom' do
+        expect { board.place(1, '☑') }.to change { board.instance_variable_get(:@board)[5][0] }.to('☑')
+      end
+    end
+
+    context 'when column is not empty' do
+      subject(:board) { described_class.new([
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☐','☐','☐','☐','☐','☐'],
+        ['☐','☑','☐','☐','☒','☐','☐'],
+      ]) }
+
+      it 'drops circle to deepest empty spot' do
+        expect { board.place(2, '☑') }.to change { board.instance_variable_get(:@board)[4][1] }.to('☑')
+      end
+    end
+  end
+
+  describe '#take_input' do
+    before do
+      allow(board).to receive(:gets).and_return('1')
+    end
+
+    context 'when requesting column number' do
+      it 'requests input with gets' do
+        expect(board).to receive(:gets)
+        board.take_input
+      end
+
+      it 'calls #place with col_input' do
+        expect(board).to receive(:place).with(1, '☑')
+        board.take_input
+      end
+    end
+
+    context 'when it is the first turn' do
+      it 'calls place with ☑' do
+        expect(board).to receive(:place).with(1, '☑')
+        board.take_input
+      end
+    end
+
+    context 'when it is the second turn' do
+      it 'calls place with ☒' do
+        board.take_input
+        expect(board).to receive(:place).with(1, '☒')
+        board.take_input
+      end
+    end
+
+    context 'when user inputs exit' do
+      before do
+        allow(board).to receive(:gets).and_return('exit')
+      end
+
+      it 'calls exit' do
+        expect(board).to receive(:exit)
+        board.take_input
+      end
+    end
+
+    context 'when user inputs q' do
+      before do
+        allow(board).to receive(:gets).and_return('q')
+      end
+
+      it 'calls exit' do
+        expect(board).to receive(:exit)
+        board.take_input
+      end
+    end
+
+    describe 'invalid inputs' do
+      before do
+        allow(board).to receive(:gets).and_return('hamster')
+      end
+      
+      it 'does not call #place' do
+        expect(board).not_to receive(:place)
+        board.take_input
+      end
+
+      context 'when user inputs letter' do
+        before do
+          allow(board).to receive(:gets).and_return('g')
+        end
+
+        it 'displays error message' do
+          expect(board).to receive(:puts).with("Invalid Input: Please input a number between 1 and 7")
+          board.take_input
+        end
+      end
+
+      context 'when user inputs large number' do
+        before do
+          allow(board).to receive(:gets).and_return('15')
+        end
+
+        it 'displays error message' do
+          expect(board).to receive(:puts).with("Invalid Input: Please input a number between 1 and 7")
+          board.take_input
+        end
+      end
+
+      context 'when user inputs zero' do
+        before do
+          allow(board).to receive(:gets).and_return('0')
+        end
+
+        it 'displays error message' do
+          expect(board).to receive(:puts).with("Invalid Input: Please input a number between 1 and 7")
+          board.take_input
+        end
+      end
+
+      context 'when user inputs negative number' do
+        before do
+          allow(board).to receive(:gets).and_return('-4')
+        end
+
+        it 'displays error message' do
+          expect(board).to receive(:puts).with("Invalid Input: Please input a number between 1 and 7")
+          board.take_input
+        end
+      end
+    end
+  end
+
+  describe '#check_for_win' do
+    before do
+      allow(board).to receive(:win)
+    end
+
+    describe 'wins' do
+      context 'horizontal win' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☒','☐','☐'],
+          ['☑','☑','☑','☑','☒','☒','☐'],
+        ]) }
+
+        it 'displays winner text' do
+          expect(board).to receive(:win)
+          board.check_for_win
+        end
+      end
+
+      context 'vertical win' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☒','☐','☐'],
+          ['☑','☐','☐','☐','☒','☒','☐'],
+        ]) }
+
+        it 'displays winner text' do
+          expect(board).to receive(:win)
+          board.check_for_win
+        end
+      end
+
+      context 'diaganol win' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☐','☑','☐','☐','☐','☐','☐'],
+          ['☐','☐','☑','☐','☒','☐','☐'],
+          ['☐','☐','☐','☑','☒','☒','☐'],
+        ]) }
+
+        it 'displays winner text' do
+          expect(board).to receive(:win)
+          board.check_for_win
+        end
+      end
+    end
+
+    describe 'losses' do
+      context 'horizontal loss' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☑','☐','☑','☒','☒','☐'],
+        ]) }
+
+        it 'does not display winner text' do
+          expect(board).not_to receive(:win)
+          board.check_for_win
+        end
+      end
+
+      context 'vertical loss' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☒','☒','☐'],
+        ]) }
+
+        it 'does not displays winner text' do
+          expect(board).not_to receive(:win)
+          board.check_for_win
+        end
+      end
+
+      context 'diaganol loss' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☑','☐','☐','☐','☐'],
+          ['☐','☐','☐','☑','☒','☒','☐'],
+        ]) }
+
+        it 'does not displays winner text' do
+          expect(board).not_to receive(:win)
+          board.check_for_win
+        end
+      end
+
+      context 'edge loss' do
+        subject(:board) { described_class.new([
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☐','☐','☐','☐','☐','☐','☐'],
+          ['☑','☐','☐','☐','☐','☐','☐'],
+          ['☑','☒','☐','☐','☐','☐','☐'],
+          ['☑','☒','☐','☐','☐','☐','☐'],
+        ]) }
+
+        it 'does not displays winner text' do
+          expect(board).not_to receive(:win)
+          board.check_for_win
+        end
+      end
+    end
+  end
+
+  describe '#play' do
+    before do
+      allow(board).to receive(:print)
+      allow(board).to receive(:take_input)
+      allow(board).to receive(:place)
+      allow(board).to receive(:check_for_win).and_return(true)
+    end
+
+    context 'when game starts' do
+      it 'calls print' do
+        expect(board).to receive(:print)
+        board.play
+      end
+
+      it 'calls take_input' do
+        expect(board).to receive(:take_input)
+        board.play
+      end
+
+      it 'calls check_for_win' do
+        expect(board).to receive(:check_for_win)
+        board.play
+      end
+    end
+  end
+
+  describe '#win' do
+    before do
+      allow(board).to receive(:print)
+      allow(board).to receive(:puts)
+    end
+
+    it 'calls print' do
+      expect(board).to receive(:print)
+      board.win('☑')
+    end
+
+    context 'when ☑ wins' do
+      it 'displays winner text' do
+        expect(board).to receive(:puts).with("\n☑ Connected Four!")
+        board.win('☑')
+      end
+    end
+
+    context 'when ☒ wins' do
+      it 'displays winner text' do
+        expect(board).to receive(:puts).with("\n☒ Connected Four!")
+        board.win('☒')
       end
     end
   end
